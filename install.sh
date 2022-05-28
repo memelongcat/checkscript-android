2d8dec3268985e5c11dfa0435f6a99001b61cecb
memelongcat <45173753+memelongcat@users.noreply.github.com>
2022-05-28 21:13:55 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,3 @@ pkg install gnupg
 pkg install unrar
 mkdir ~/storage/downloads/chek
 gpg --import public.gpg
-echo -e "cd ~/storage/download/chek\nunrar e *.jpg\ngpg --verify *jpg.gpg\nsleep 30\nrm -i *.jpg *.jpg.gpg" >> checkscript.sh
-chmod +x checkscript.sh

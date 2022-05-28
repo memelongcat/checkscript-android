4eb9f752dd0534c42e5716e2db36f9c84929b9f5
memelongcat <45173753+memelongcat@users.noreply.github.com>
2022-05-28 21:05:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 termux-setup-storage
-pkg install gnupg unrar
+pkg install gnupg
+pkg install unrar
 mkdir ~/storage/downloads/chek
 gpg --import public.gpg
 echo -e "cd ~/storage/download/chek\nunrar e *.jpg\ngpg --verify *jpg.gpg\nsleep 30\nrm -i *.jpg *.jpg.gpg" >> checkscript.sh
